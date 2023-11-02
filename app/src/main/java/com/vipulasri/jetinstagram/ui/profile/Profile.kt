51cca9ab5c900e428cf18ec68587fa54d97fe130
samuelbassett <samuelmbassett@gmail.com>
2023-11-02 07:55:49 -0400

    Implemented Post, Favorite, and Profile Screens

--- app/src/main/java/com/vipulasri/jetinstagram/ui/profile/Profile.kt
+++ app/src/main/java/com/vipulasri/jetinstagram/ui/profile/Profile.kt
@@ -6,17 +6,13 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.Divider
 import androidx.compose.material.Icon
@@ -30,16 +26,11 @@ import androidx.compose.material.Text
 import androidx.compose.material.rememberScaffoldState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
-import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
@@ -51,7 +42,6 @@ import com.vipulasri.jetinstagram.data.PostsRepository
 import com.vipulasri.jetinstagram.model.Post
 import com.vipulasri.jetinstagram.model.User
 import com.vipulasri.jetinstagram.ui.components.PostGrid
-import kotlin.math.min
 
 @Composable
 fun Profile(
@@ -130,7 +120,7 @@ fun ProfileInfo(profile: MutableState<List<Post>>) {
     Row(
         modifier = Modifier
             .fillMaxWidth()
-            .padding(top = 8.dp, start = 16.dp, end = 16.dp),
+            .padding(top = 8.dp, start = 16.dp, end = 16.dp, bottom = 8.dp),
         horizontalArrangement = Arrangement.SpaceBetween,
         verticalAlignment = Alignment.CenterVertically
     ) {
@@ -141,7 +131,7 @@ fun ProfileInfo(profile: MutableState<List<Post>>) {
                 .clip(CircleShape)
         ) {
             Image(
-                painter = rememberImagePainter(profile.value[1].image),
+                painter = rememberImagePainter(profile.value[1].user.image),
                 contentDescription = null,
                 modifier = Modifier.fillMaxSize()
             )
